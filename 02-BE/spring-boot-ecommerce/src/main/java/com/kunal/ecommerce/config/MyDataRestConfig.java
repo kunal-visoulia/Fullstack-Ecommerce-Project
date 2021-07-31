7c76424baa00d089e463d72d0c8137acdc1c4740
k_visoulia <k_visoulia@dellteam.com>
2021-07-31 21:24:58 +0530

    Remove unused imports

--- 02-BE/spring-boot-ecommerce/src/main/java/com/kunal/ecommerce/config/MyDataRestConfig.java
+++ 02-BE/spring-boot-ecommerce/src/main/java/com/kunal/ecommerce/config/MyDataRestConfig.java
@@ -2,7 +2,6 @@ package com.kunal.ecommerce.config;
 
 import com.kunal.ecommerce.entity.Product;
 import com.kunal.ecommerce.entity.ProductCategory;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -14,7 +13,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
-        HttpMethod[] unsupportedActions = {HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE}
+        HttpMethod[] unsupportedActions = {HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE};
         config.getExposureConfiguration()
                 .forDomainType(Product.class) //disabling for product Repo
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)) //for a singe item
