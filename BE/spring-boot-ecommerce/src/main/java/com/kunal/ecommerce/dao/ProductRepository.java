bbc19eca1bdb59669065267ad5fe2084d61d460c
k_visoulia <k_visoulia@dellteam.com>
2021-08-01 15:29:31 +0530

    added query method in BE and search componenet in FE for searching products using keyword

--- BE/spring-boot-ecommerce/src/main/java/com/kunal/ecommerce/dao/ProductRepository.java
+++ BE/spring-boot-ecommerce/src/main/java/com/kunal/ecommerce/dao/ProductRepository.java
@@ -10,4 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable); // containing is similar to LIKE
+    // SELECT * FROM PRODUCT p WHERE p.name LIKE CONCAT('%',:name,'%')
+
 }
--- FE/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ FE/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit {
   products: Product[];
   currentCategoryId: number;
   currentCategoryName: string;
+  searchMode: boolean;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -25,22 +26,41 @@ export class ProductListComponent implements OnInit {
 
   listProducts() {
 
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    }
+    else {
+      this.handleListProducts();
+    }
+
+  }
+
+  handleSearchProducts() {
+
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
+
+    // now search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+
+  handleListProducts() {
+
     // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
     if (hasCategoryId) {
       // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
-
-      // get the "name" param string
-      this.currentCategoryName = this.route.snapshot.paramMap.get('name');
     }
     else {
       // not category id available ... default to category id 1
       this.currentCategoryId = 1;
-
-      this.currentCategoryName = 'Books';
-
     }
 
     // now get the products for the given category id
@@ -48,7 +68,6 @@ export class ProductListComponent implements OnInit {
       data => {
         this.products = data;
       }
-    )
+    )    
   }
-
 }
