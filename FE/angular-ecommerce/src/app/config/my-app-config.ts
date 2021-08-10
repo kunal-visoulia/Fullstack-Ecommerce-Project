bab2f4b71bd56b00325a9303a307d6c323ba352f
k_visoulia <k_visoulia@dellteam.com>
2021-08-10 22:13:08 +0530

    Secure communication with HTTPS p3

--- FE/angular-ecommerce/src/app/config/my-app-config.ts
+++ FE/angular-ecommerce/src/app/config/my-app-config.ts
@@ -6,7 +6,7 @@ export default {
     oidc: {
         clientId: '0oa1fm73gegGWzrQ65d7', //public id of client app
     /*issuer of tokens */    issuer: 'https://dev-57464947.okta.com/oauth2/default', // URL when authorizing with okta Authorization Server
-        redirectUri: 'http://localhost:4200/login/callback', // send them here after user logins
+        redirectUri: 'https://localhost:4200/login/callback', // send them here after user logins
         scopes: ['openid', 'profile', 'email'] // scopes provide acces to info about the user(openid: required for authentication requests)
     }
 
